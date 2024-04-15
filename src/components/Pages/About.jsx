3c8d8d1638bf8ee43e7c98f43cb34be5cc07c4e3
Koleton Murray <106117059+koletonmurray@users.noreply.github.com>
2024-04-14 23:19:07 -0600

    about page has content

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -5,17 +5,22 @@ export default function About () {
         <div className="px-10">
             <h1>History of <Logo/></h1>
             <div className="text-left max-w-5xl mx-auto text-black py-10">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget ligula eu lectus lobortis condimentum. Aliquam nonummy auctor massa. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nulla at risus. Quisque purus magna, auctor et, sagittis ac, posuere eu, lectus. Nam mattis, felis ut adipiscing.
+                <p className="pb-5">
+                    The <Logo/> was incorporated in 1977 and was known as Eves Electric Inc. In 1986 the corporate name was changed to
+                    the B.L. Murray Corp due to a buyout. In 2024 it has since been rebranded as <Logo/> In our decades of history,
+                    we have been involved in Commercial, Industrial, and Residential Electrical. We have worked on all types of projects
+                    ranging from schools, to waste water treatment plants, to offices, to manufacturing buildings, and storage buildings.
+                    We have worked on High Voltage, Low Voltage, and Data wiring.
                 </p>
-                <p>
-                    Interdum malesuada fames ac ante ipsum primis in faucibus. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo non, facilisis at, elementum in, turpis.
+                <p className="pb-5">
+                    Currently we have specialized in Commercial and Industrial Electrical work. Our work can be found thousands of buildings 
+                    across the state of Utah. While primarily working in Utah County, we have worked projects in most of Utah's major areas.
+                    Noteworthy clients of ours have been Brigham Young University, Utah Valley University, the Church of Jesus Christ of Latter-day
+                    Saints, the United States Air Force, and the Utah National Guard. We have worked on projects for city, county, and state 
+                    buildings.
                 </p>
-                <p>
-                    Vestibulum et mauris vel ante finibus maximus nec ut leo. Integer consectetur lorem et purus rhoncus, in aliquet massa blandit. Morbi tincidunt, dui sit amet facilisis feugiat, odio metus gravida ante, eget lacinia odio sem nec elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
-                </p>
-                <p>
-                    Curabitur pretium tincidunt lacus. Nulla gravida orci a odio. Nullam varius, turpis et commodo pharetra, est eros bibendum elit, nec luctus magna felis sollicitudin mauris. Integer in mauris eu nibh euismod gravida. Duis ac tellus et risus vulputate vehicula. Donec lobortis risus a elit. Etiam tempor. Ut ullamcorper, ligula eu tempor congue, eros est euismod turpis, id tincidunt sapien risus a quam. Maecenas fermentum consequat mi. Donec fermentum.
+                <p className="pb-5">
+                    <Logo/> is committed to quality work and powering our community through its electrical work!
                 </p>
             </div>
         </div>
